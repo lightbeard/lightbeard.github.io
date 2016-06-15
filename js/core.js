e047a509d8397acdda74fc2527d27b7effc0570d
lightbeard <lightbeard@pablanca.com>
2016-06-15 10:03:20 -0600

    menu working again

--- js/core.js
+++ js/core.js
@@ -1,16 +1,48 @@
 
-$('#backup-menu').css('display','none');
+window.addEventListener('WebComponentsReady', function(e) {
+	// imports are loaded
+	var importLink = $('link[href="imports.html"]');
+	var menuHtml = $('#dock-menu', importLink[0].import);
+	var footer = $('#footer', importLink[0].import);
+	$('#main-div').prepend(menuHtml);
+	$('#main-div').append(footer);
+	
+	if(!bowser.mobile && !bowser.tablet) {
+		$('#backup-menu').css('display','none');
 
-$('#dock').css('display','block').Fisheye(
-	{
-		maxWidth: 100,
-		items: 'a',
-		itemsText: 'span',
-		container: '.dock-container',
-		itemWidth: 80,
-		proximity: 100,
-		halign : 'center'
+		$('#dock').css('display','block').Fisheye(
+			{
+				maxWidth: 100,
+				items: 'a',
+				itemsText: 'span',
+				container: '.dock-container',
+				itemWidth: 80,
+				proximity: 100,
+				halign : 'center'
+			}
+		);
+	} else {
+		// possible screen to small for menu
 	}
-);
+	
+	$('p').hyphenate('en-us');
 
-$('p').hyphenate('en-us');
+	$('.carousel').slick({
+		dots: true,
+		autoplay: true,
+		autoplaySpeed: 5000,
+		pauseOnDotsHover: true,
+		pauseOnFocus: false,
+		pauseOnHover: false,
+		infinite: true,
+		fade: true,
+		cssEase: 'linear',
+		speed: 500
+	});
+		
+	$('.slick-dots').find('button').on('click', function () {
+		$('.carousel').slick('slickPause');
+		$('.carousel').slick('setOption', 'autoplay', false, false);
+	});
+	
+});
